5535d41cb40a41640801ee3b3bacc2e37fd279da
vetradev <vetradev@gmail.com>
2025-01-21 22:34:39 +0530

    Made Floating Animation Better

--- app/components/MainTwo.jsx
+++ app/components/MainTwo.jsx
@@ -1,72 +1,95 @@
 import React, { useState, useEffect } from "react";
 import "../styles/MainTwo.css";
-import image1 from "../../public/assets/image1.jpg";
+import image1 from "../assets/image1.jpg";
 
 export default function MainTwo() {
-    const [tagText, setTagText] = useState("get inspired");
-    const [fadeKey, setFadeKey] = useState(0);
+  const [tagText, setTagText] = useState("get inspired");
+  const [fadeKey, setFadeKey] = useState(0);
 
-    useEffect(() => {
-        const textList = [
-            "get inspired",
-            "save websites",
-            "create moodboards",
-            "develop"
-        ];
+  useEffect(() => {
+    const textList = [
+      "get inspired",
+      "save websites",
+      "create collections",
+      "execute more",
+    ];
 
-        let index = 0;
+    let index = 0;
 
-        const intervalId = setInterval(() => {
-            index = (index + 1) % textList.length;
-            setTagText(textList[index]);
-            setFadeKey((prevKey) => prevKey + 1);
-        }, 4000);
+    const intervalId = setInterval(() => {
+      index = (index + 1) % textList.length;
+      setTagText(textList[index]);
+      setFadeKey((prevKey) => prevKey + 1);
+    }, 4000);
 
-        return () => clearInterval(intervalId);
-    }, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    return (
-        <div className="main-two-container">
-            <div className="main-two">
-                <div className="content-wrapper">
-                    <h1>
-                        Browse through a curated <br /> collection of resources
-                    </h1>
-                    <div className="sub-heading">
-                        Use Vetra to{" "}
-                        <span className="notes-tag">
-                            <span id="notes-animate" key={fadeKey}>
-                                {tagText}
-                            </span>
-                        </span>
-                    </div>
-                </div>
-                <div className="gallery-container">
-                    <div className="floating-images">
-                        <div className="floating-image image-1" data-depth="20">
-                            <img src="https://pawan-kamat-portfolio.vercel.app/images/GitaGPT-Home.png" alt="Floating 1" />
-                        </div>
-                        <div className="floating-image image-2" data-depth="40">
-                            <img src="https://pawan-kamat-portfolio.vercel.app/images/WebStyle-Home.png" alt="Floating 2" />
-                        </div>
-                        <div className="floating-image image-3" data-depth="10">
-                            <img src="https://i.pinimg.com/236x/ed/22/95/ed229537f5cb0652e27aaddd76a3e174.jpg" alt="Floating 3" />
-                        </div>
-                        <div className="floating-image image-4" data-depth="30">
-                            <img src="https://pawan-kamat-portfolio.vercel.app/images/Breather-Home.png" alt="Floating 4" />
-                        </div>
-                        <div className="floating-image image-5" data-depth="25">
-                            <img src="https://i.pinimg.com/474x/bd/1c/56/bd1c5628b26db143dff37bef1f2609a9.jpg" alt="Floating 5" />
-                        </div>
-                        <div className="floating-image image-6" data-depth="25">
-                            <img src="https://i.pinimg.com/236x/ce/f2/40/cef240a79e2fd87bb493381e474a5271.jpg" alt="Floating 6" />
-                        </div>
-                        <div className="floating-image image-7" data-depth="25">
-                            <img src={image1} alt="Floating 7" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const images = [
+    {
+      src: "https://pawan-kamat-portfolio.vercel.app/images/GitaGPT-Home.png",
+      depth: 20,
+      text: "Explore AI Tools",
+    },
+    {
+      src: "https://pawan-kamat-portfolio.vercel.app/images/WebStyle-Home.png",
+      depth: 40,
+      text: "Design Better Websites",
+    },
+    {
+      src: "https://github.com/bryanleezh/www/raw/master/images/demo/main.png",
+      depth: 10,
+      text: "Discover Live Previews",
+    },
+    {
+      src: "https://pawan-kamat-portfolio.vercel.app/images/Breather-Home.png",
+      depth: 30,
+      text: "Collaborate Effortlessly",
+    },
+    {
+      src: "https://github.com/Yuteoctober/wins95Portfolio/raw/main/src/assets/markdown.png?raw=true",
+      depth: 25,
+      text: "Markdown in Style",
+    },
+    {
+      src: "https://kmkzzsqbmjarjzfpopin.supabase.co/storage/v1/object/sign/photos/andrijaweb.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwaG90b3MvYW5kcmlqYXdlYi5wbmciLCJpYXQiOjE3MzY4NDkyNDQsImV4cCI6MTc2ODM4NTI0NH0.Jguj3KLkcM5SozoqxQu2a-iooA1ROCucyp-YG5UR_UU&t=2025-01-14T10%3A07%3A26.099Z",
+      depth: 25,
+      text: "Fast Prototyping",
+    },
+    { src: image1, depth: 25, text: "Next-Level Development" },
+  ];
+
+  return (
+    <div className="main-two-container">
+      <div className="main-two">
+        <div className="content-wrapper">
+          <h1>
+            Browse through a curated <br /> collection of resources
+          </h1>
+          <div className="sub-heading">
+            Use Vetra to{" "}
+            <span className="notes-tag">
+              <span id="notes-animate" key={fadeKey}>
+                {tagText}
+              </span>
+            </span>
+          </div>
         </div>
-    );
+        <div className="gallery-container">
+          <div className="floating-images">
+            {images.map((img, index) => (
+              <div
+                key={index}
+                className={`floating-image image-${index + 1}`}
+                data-depth={img.depth}
+              >
+                <img src={img.src} alt={`Floating ${index + 1}`} />
+                {/* <div className="floating-text">{img.text}</div> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
