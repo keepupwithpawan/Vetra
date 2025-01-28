17f91153f87d12152103917b1c755f43062c4894
Vetra <vetradev@gmail.com>
2025-01-28 22:19:31 +0530

    Update page.tsx

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -4,6 +4,7 @@ import Grid from '../Grid'
 import Navbar from '../components/Navbar'
 import NavbarHome from '../components/NavbarHome'
 
+
 const page = () => {
     const [query, setQuery] = useState<string>('');
     return (
