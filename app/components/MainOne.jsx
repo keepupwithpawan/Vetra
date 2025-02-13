7f95e9fe721251ebb54bb25877db8dfbb0bfa73b
Vetra <vetradev@gmail.com>
2025-02-13 23:57:21 +0530

    Updated MainOne.jsx

--- app/components/MainOne.jsx
+++ app/components/MainOne.jsx
@@ -2,39 +2,22 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation"
 import "../styles/MainOne.css";
 
-
-
-export default function MainOne({ onTransition }) {
+export default function MainOne() {
   const navigate = useRouter(); // Hook for navigation
 
   const handleRedirect = () => {
     navigate.push("/signup"); // Redirect to /home
   };
 
-  const [isTransitioning, setIsTransitioning] = useState(false);
-
-  const handleScroll = (e) => {
-    if (isTransitioning) return;
-
-    if (e.deltaY > 0) {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        if (onTransition) onTransition(); // Ensure onTransition is called if defined
-      }, 1500);
-    }
-  };
-
   return (
     <div
       id="main"
-      className={`main-one ${isTransitioning ? "transitioning" : ""}`}
-      onWheel={handleScroll}
     >
       <div id="lander-container">
         {/* Video as background */}
         <video src="https://res.cloudinary.com/dwkbeovcw/video/upload/v1737741388/Lander-Shapes_pz1wv4.mp4" autoPlay muted loop></video>
 
-        <div id="lander" className={isTransitioning ? "fade-out" : ""}>
+        <div id="lander">
           <p id="heading">From Exploration to Execution</p>
           <p id="sub-heading">
             Discover designs, explore source code, and customize instantly. Vetra makes your ideas actionable!
